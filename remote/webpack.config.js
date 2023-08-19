e4f6974b067389e5ee564f8b50b91756941a97de
navneet <mani@indovations.in>
2023-08-19 22:28:35 +0530

    solid js working fine issue with react have to check

---
+++ remote/webpack.config.js
@@ -0,0 +1,61 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const deps = require("./package.json").dependencies;
+module.exports = (_, argv) => ({
+  output: {
+    publicPath: "http://localhost:3030/",
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+  },
+
+  devServer: {
+    port: 3030,
+    historyApiFallback: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "remote",
+      filename: "remoteEntry.js",
+      remotes: {},
+      exposes: {"./Counter":"./src/Counter.jsx",
+      "./counterWapper":"./src/counterWapper.jsx"},
+      shared: {
+        ...deps,
+        "solid-js": {
+          singleton: true,
+          requiredVersion: deps["solid-js"],
+        },
+      },
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+});
